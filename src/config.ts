e98b79342340d222d6d65108582412a00a6bf789
Jeff <110354170+becausebirds@users.noreply.github.com>
2023-12-16 09:20:47 -0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Jeff Mann | Marketing Analytics Manager and Birdwatcher';
+export const SITE_DESCRIPTION = 'Jeff Mann is seasoned marketing analytics professional and birdwatcher in San Antonio, TX';
