225de34bf692b36d83bcc9dce8418e0aab539324
Jeff Mann <110354170+becausebirds@users.noreply.github.com>
2023-12-16 10:37:45 -0600

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,6 @@ import { z, defineCollection } from "astro:content";
 const blogSchema = z.object({
     title: z.string(),
     description: z.string(),
-    postURL: z.string().url(),
     pubDate: z.coerce.date(),
     updatedDate: z.string().optional(),
     heroImage: z.string().optional(),
